31f0cfd219dca78f386e0c8b361c07d855c3f88a
jahentao <tzhltjh@163.com>
2020-03-12 10:43:25 +0800

    faas gateway build.sh & push.sh

--- gateway/push.sh
+++ gateway/push.sh
@@ -7,6 +7,8 @@ export eTAG="latest-dev"
 
 if [ "$arch" = "armv7l" ] ; then
    eTAG="latest-armhf-dev"
+elif [ "$arch" = "aarch64" ]; then
+   eTAG="latest-arm64-dev"
 fi
 
 echo "$1"
@@ -14,10 +16,14 @@ if [ "$1" ] ; then
   eTAG=$1
   if [ "$arch" = "armv7l" ] ; then
     eTAG="$1-armhf"
+  elif [ "$arch" = "aarch64" ]; then
+    eTAG="$1-arm64"
   fi
 fi
 
-echo Pushing openfaas/gateway:$eTAG
+NS=jahentao
 
-docker push openfaas/gateway:$eTAG
+echo Pushing $NS/gateway:$eTAG
+
+docker push $NS/gateway:$eTAG
 
